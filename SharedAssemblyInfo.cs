1fdef724bab421b21a2adbc35abdb770c09cb569
JakobPer <jakob.pernsteiner@tuwien.ac.at>
2022-06-07 15:30:03 +0200

    moved sharedAssemblyInfo to subrepo

---
+++ SharedAssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyCompany("TU Wien (Vienna University of Technology)")]
+[assembly: AssemblyProduct("SIMULTAN")]
+[assembly: AssemblyCopyright("Copyright (c) SIMULTAN User Group")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("0.2.156.*")]
+
+[assembly: InternalsVisibleTo("SIMULTAN.Tests")] 
