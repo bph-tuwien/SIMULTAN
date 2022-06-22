9396783d1acb024cd0a5c028c296d2533c0bb382
JakobPer <jakob.pernsteiner@tuwien.ac.at>
2022-06-22 10:05:33 +0200

    added debug messages to release script

--- make_release.ps1
+++ make_release.ps1
@@ -1,3 +1,8 @@
+if ($null -eq $Env:GIT_CRED_PSW -or '' -eq $Env:GIT_CRED_PSW) {
+    Write-Host("Environment variable GIT_CRED_PSW not set, aborting.")
+    exit -1
+}
+
 # Set TLS and SSL versions cause Jenkins needs it
 [Net.ServicePointManager]::SecurityProtocol = "Tls12, Tls11, Tls, Ssl3"
 # Path to SIMULTAN DLL
@@ -5,6 +10,8 @@ $path = Join-Path $(Get-Location).Path 'SIMULTAN\bin\Release\SIMULTAN.dll'
 # Load the assembly into memory so we don't get a file lock
 $assembly = [Reflection.Assembly]::Load([IO.File]::ReadAllBytes($path))
 $version = $assembly.GetName().Version.ToString()
+
+Write-Host("Creating release...")
 
 # Make the GitHub release, push DLL later
 $response = Invoke-WebRequest -Method 'POST' -Headers @{'Accept' ='application/vnd.github.v3+json';'Authorization' = "token $Env:GIT_CRED_PSW";} `
@@ -15,7 +22,13 @@ $response = Invoke-WebRequest -Method 'POST' -Headers @{'Accept' ='application/v
 $parsed = ConvertFrom-Json($response.Content)
 $id = $parsed.id
 
+Write-Host("Created release with id $id")
+
+Write-Host("Uploading DLL...")
+
 # Push DLL to release assets
 $response = Invoke-WebRequest -Method 'POST' -Headers @{'Accept' ='application/vnd.github.v3+json';'Authorization' = "token $Env:GIT_CRED_PSW";} `
     -InFile $path -ContentType 'applicateion/octet-stream' `
     -Uri "https://uploads.github.com/repos/bph-tuwien/$Env:REPO_NAME/releases/$id/assets?name=SIMULTAN.dll"
+
+Write-Host("Release done.")
